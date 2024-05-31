368a7ae9b7aa50fabbac8bc4d398b6108f0073d0
James <james.waugh.r@gmail.com>
2024-05-31 16:02:14 -0400

    Work a ton on getting simulation working

--- src/simavr-toolbox/sim_tlc59116.cpp
+++ src/simavr-toolbox/sim_tlc59116.cpp
@@ -1,32 +1,92 @@
 #include "sim_tlc59116.hpp"
 
+#include <array>
+#include <cstdint>
 #include <cstdlib>
-#include <iostream>
-#include <ostream>
 
 #include "avr_twi.h"
+#include "sim_base.hpp"
 #include "sim_i2c_base.hpp"
-#include "sim_irq.h"
-#include "sim_time.h"
 
-SimTLC59116::SimTLC59116(avr_t* avr, uint8_t i2cAddress) : SimAvrI2CComponent(avr, i2cAddress) {}
+SimTLC59116::SimTLC59116(avr_t* avr, uint8_t i2cAddress) : SimAvrI2CComponent(avr, i2cAddress) {
+  Registers_.fill(0);
+}
 
-void SimTLC59116::HandleI2CMessage(avr_twi_msg_t msg) {
-  if (msg.msg & TWI_COND_STOP) {
-    ExecuteCommand();
-  } else if (msg.msg & TWI_COND_START) {
-    CommandBuffer_.clear();
-  } else if (msg.msg & TWI_COND_WRITE) {
-    CommandBuffer_.push_back(msg.data);
-  } else {
+void SimTLC59116::HandleI2CMessage(avr_twi_msg_t message) {
+  if (message.msg & TWI_COND_START) {
+    if (State_ == State::Start) {
+      auto mode_ = static_cast<I2CMode>(message.addr & 1);
+      if (mode_ == I2CMode::WRITE) {
+        State_ = State::GiveRegisterAddress;
+      } else {
+        std::abort();
+      }
+    } else {
+      std::abort();
+    }
+    SendToAvrI2CAck();
+  } else if (message.msg & TWI_COND_STOP) {
+    Reset();
+  } else if (message.msg & TWI_COND_WRITE) {
+    SendToAvrI2CAck();
+    if (State_ == State::GiveRegisterAddress) {
+      AutoIncrement_ = (message.data & 0xE0) != 0;  // TODO support other AI modes
+      SelectedRegister_ = message.data & 0x1F;
+      State_ = State::WriteRegister;
+    } else if (State_ == State::WriteRegister) {
+      // They decided to write a register, do it
+      Registers_.at(SelectedRegister_) = message.data;
+      if (AutoIncrement_) {
+        SelectedRegister_ += 1;
+        // TODO support other AI modes
+        if (SelectedRegister_ > 0x1B) {
+          SelectedRegister_ = 0;
+        }
+      }
+      debug_log("Wrote Register %02x = %02X\n", SelectedRegister_, message.data);
+    }
+  } else if (message.msg & TWI_COND_READ) {
     std::abort();
   }
 }
 
-void SimTLC59116::ExecuteCommand() {
-  // use CommandBuffer_
-}
-
 void SimTLC59116::Reset() {
   CommandBuffer_.clear();
+  AutoIncrement_ = false;
+  SelectedRegister_ = 0;
+}
+
+enum class LedState : uint8_t {
+  Off = 0,
+  FullOn = 1,
+  Pwm = 2,
+  PwmGroup = 3,
+};
+
+std::array<uint8_t, 16> SimTLC59116::GetCurrentState() const {
+  std::array<uint8_t, 16> value;
+
+  for (int i = 0; i < 16; ++i) {
+    const uint8_t reg = i / 4;
+    const uint8_t bit = i % 4;
+    auto reg_2 = (Registers_.at(0x14 + reg) >> (2 * bit)) & 0x03;
+    const auto status = static_cast<LedState>(reg_2);
+    switch (status) {
+      case LedState::Off:
+        value[i] = 0;
+        break;
+      case LedState::FullOn:
+        value[i] = 1;
+        break;
+      case LedState::Pwm:
+        value[i] = Registers_.at(0x02 + i);
+        break;
+      case LedState::PwmGroup:
+        std::abort();
+        // value[i] = Registers_.at(0x02 + 1);
+        break;
+    }
+  }
+
+  return value;
 }
--- src/simavr-toolbox/sim_tlc59116.hpp
+++ src/simavr-toolbox/sim_tlc59116.hpp
@@ -12,9 +12,17 @@ class SimTLC59116 final : public SimAvrI2CComponent {
   SimTLC59116(avr_t* avr, uint8_t i2cAddress);
   virtual void HandleI2CMessage(avr_twi_msg_t msg) override;
   virtual void Reset() override;
-  std::array<bool, 16> GetCurrentState() const;
+  std::array<uint8_t, 16> GetCurrentState() const;
 
  private:
-  void ExecuteCommand();
+  enum class State {
+    Start,
+    GiveRegisterAddress,
+    ReadRegister,
+    WriteRegister,
+  } State_;
   std::vector<uint8_t> CommandBuffer_;
+  std::array<uint8_t, 0x1E> Registers_;
+  uint8_t SelectedRegister_;
+  bool AutoIncrement_;
 };
